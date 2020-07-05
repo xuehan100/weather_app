412cec69ea2b3852694fc6b89c1c67d51218c213
xuehan <xuehan868@gmail.com>
2020-07-04 22:33:26 -0700

    first commit

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/xuehan/Developer/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/xuehan/AndroidStudioProjects/Clima-Flutter-master"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/Users/xuehan/Developer/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
